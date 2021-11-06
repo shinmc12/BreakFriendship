4852f5b6b4da9a904b1241b0871d455c69201139
Hong Boem Sun <qjatjs123123@naver.com>
2021-11-06 18:52:24 +0900

    채팅기능 및 대기방

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -22,9 +22,15 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     [Header("RoomPanel")]
     public GameObject RoomPanel;
+    public Text ListText;
+    public Text RoomInfoText;
+    public Text[] ChatText;
+    public InputField ChatInput;
 
     [Header("ETC")]
     public Text StatusText;
+    public PhotonView PV ;
+
 
     int Max_Player = 0;
 
@@ -104,6 +110,8 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     //JoinLobby() 콜백함수로 실행
     public override void OnJoinedLobby()
     {
+        Debug.Log("로비참가");
+        
         LobbyPanel.SetActive(true);
         PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
         WelcomeText.text = PhotonNetwork.LocalPlayer.NickName + "님 환영합니다.";
@@ -128,6 +136,8 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         }
         else
         {
+            Debug.Log("방생성");
+            
             // 방이름을 RoomInput.text로 방옵션을 최대인원수 2로 성공적으로 수행되면 OnJoinedRoom 콜백함수실행
             if(Max_Player == 2)
                 PhotonNetwork.CreateRoom(RoomInput.text, new RoomOptions { MaxPlayers = 2 });
@@ -156,11 +166,64 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public void LeaveRoom() => PhotonNetwork.JoinRandomRoom();
 
-    public override void OnJoinedRoom()
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        RoomPanel.SetActive(true);
+        RoomRenewal();
+        PV.RPC("ChatRPC", RpcTarget.All, "<color=yellow>" + newPlayer.NickName + "님이 참가하셨습니다</color>");
+        Debug.Log("참가");
+
 
     }
 
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        RoomRenewal();
+        PV.RPC("ChatRPC", RpcTarget.All, "<color=yellow>" + otherPlayer.NickName + "님이 퇴장하셨습니다</color>");
+    }
+
+    void RoomRenewal()
+    {
+        ListText.text = "";
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+            ListText.text += PhotonNetwork.PlayerList[i].NickName + ((i + 1 == PhotonNetwork.PlayerList.Length) ? "" : ", ");
+        RoomInfoText.text = PhotonNetwork.CurrentRoom.Name + " / " + PhotonNetwork.CurrentRoom.PlayerCount + "명 / " + PhotonNetwork.CurrentRoom.MaxPlayers + "최대";
+    }
+
+
+    public override void OnJoinedRoom()
+    {
+        RoomPanel.SetActive(true);
+        RoomRenewal();
+        ChatInput.text = "";
+        for (int i = 0; i < ChatText.Length; i++) ChatText[i].text = "";
+        Debug.Log("방참가");
+    }
+
+    public void Send()
+    {
+        string msg = PhotonNetwork.NickName + " : " + ChatInput.text;
+        PV.RPC("ChatRPC", RpcTarget.All, PhotonNetwork.NickName + " : " + ChatInput.text);
+        ChatInput.text = "";
+        Debug.Log("send");
+    }
+
+    [PunRPC] // RPC는 플레이어가 속해있는 방 모든 인원에게 전달한다
+    void ChatRPC(string msg)
+    {
+        bool isInput = false;
+        for (int i = 0; i < ChatText.Length; i++)
+            if (ChatText[i].text == "")
+            {
+                isInput = true;
+                ChatText[i].text = msg;
+                break;
+            }
+        if (!isInput) // 꽉차면 한칸씩 위로 올림
+        {
+            for (int i = 1; i < ChatText.Length; i++) ChatText[i - 1].text = ChatText[i].text;
+            ChatText[ChatText.Length - 1].text = msg;
+        }
+    }
 
 }
